8acf42b11b8392e7e4b284a0824ba49e2ef3da69
Matt <moddell@hotmail.co.uk>
2022-02-12 12:57:16 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Code_Abbey_Solutions
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Problem1());  
+
+        }
+
+        static int Problem1()
+        {
+            List<int> intData = Utilities.FetchProblemData("1");
+
+            int solution = intData.Sum();
+
+            return solution;
+        }
+        
+        static int Problem2()
+        {
+            List<int> intData = Utilities.FetchProblemData("2");
+
+            int solution = intData.Sum();
+
+            return solution;
+
+        }
+    }
+}
---
+++ Utilities.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Abbey_Solutions
+{
+    public class Utilities
+    {
+        
+        public static string[] ReadFile(string problem)
+        {
+            string absolutePath = @"C:\Users\Matt\source\repos\Code Abbey Solutions";
+            string input = System.IO.File.ReadAllText(absolutePath+@$"\Text Files\Problem {problem}.txt");
+
+            string[] output = input.Split(" ");
+            return output;
+        }
+
+        public static List<int> FetchProblemData(string problemNo)
+        {
+            List<int> intData = new List<int>();
+            string[] stringData = ReadFile(problemNo);
+            foreach (string number in stringData)
+            {
+                intData.Add(Int32.Parse(number));
+            }
+
+            return intData;
+        }
+    }
+}
